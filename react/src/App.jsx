f5bc3c6657bb8ca0d6415ece003f7e7272b6113f
abhishek patil <abhishekpatil1501@gmail.com>
2024-11-29 22:04:58 +0530

    cards using map

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Iphone from './assests/images/iphone16.jpeg'
+import './style.css'
+export function App(){
+  console.log(Iphone);
+  
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card">
+      <img src={Iphone} alt="" />
+      <div className='infor'>
+        <h4>Iphone 16 pro max</h4>
+        <p>Lorem, ipsum dolor.</p>
+        <p>$100</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+    
     </>
+   
+   
   )
+
 }
 
-export default App
+
