9f8f84a6fd6ef5a756bfcc3f01354a1150202019
abhishek patil <abhishekpatil1501@gmail.com>
2024-11-30 19:49:07 +0530

    practice on react router

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,19 +2,9 @@ import Iphone from './assests/images/iphone16.jpeg'
 import './style.css'
 export function App(){
   console.log(Iphone);
-  
-
-  
   return(
     <>
-    <div className="card">
-      <img src={Iphone} alt="" />
-      <div className='infor'>
-        <h4>Iphone 16 pro max</h4>
-        <p>Lorem, ipsum dolor.</p>
-        <p>$100</p>
-      </div>
-    </div>
+    
     
     </>
    
